7a5388c64acecdc77763d1af1534a569a583e136
HRegniez <henriregniez124@gmail.com>
2023-01-01 16:00:04 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,16 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
+    fontFamily: {
+      'display': ['karla'],
+    },
     extend: {
       colors: {
         prim1: 'hsl(179, 62%, 43%)',
-        prim2: 'hsl(71, 73%, 54%)',
-        neut1: 'hsl(204, 43%, 93%)',
-        neut2: 'hsl(218, 22%, 67%)',
+        prim2: 'hsl(179, 62%, 53%)',
+        prim3: 'hsl(71, 73%, 54%)',
+        prim4: 'hsl(204, 43%, 93%)',
+        prim5: 'hsl(218, 22%, 67%)',
       },
     },
     plugins: [],
